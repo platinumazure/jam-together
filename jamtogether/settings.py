31162e41f7399734d226df90eb29e26125c2193f
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2020-06-19 17:44:37 -0500

    Switch to limit/offset pagination

--- jamtogether/settings.py
+++ jamtogether/settings.py
@@ -45,7 +45,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
+        'rest_framework_json_api.pagination.JsonApiLimitOffsetPagination',
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_json_api.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
@@ -73,6 +73,9 @@ REST_FRAMEWORK = {
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
 }
+
+JSON_API_FORMAT_FIELD_NAMES = 'camelize'
+JSON_API_FORMAT_TYPES = 'camelize'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
