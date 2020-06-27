8231e5bd42a3487febbe812222c14b08d16a3299
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2020-06-27 17:56:39 -0500

    Add url field for JamSession detail relationship

--- apps/jtapi/serializers.py
+++ apps/jtapi/serializers.py
@@ -2,6 +2,8 @@ from rest_framework_json_api import serializers
 from .models import JamSession
 
 class JamSessionSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='jamsession-detail')
+
     class Meta:
         model = JamSession
         fields = '__all__'
