b0d6184b3c122d57ff736599e03545e2fb8a533a
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2020-07-05 13:42:33 -0500

    Add JamSessionMembership model, migration, and foreign key field

--- apps/jtapi/models.py
+++ apps/jtapi/models.py
@@ -7,8 +7,20 @@ class JamSession(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
     conductor = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
+    members = models.ManyToManyField(
+        User,
+        related_name='jam_session_memberships',
+        through='JamSessionMembership'
+    )
 
     created_by = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+class JamSessionMembership(models.Model):
+    jam_session = models.ForeignKey(JamSession, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} in {self.jam_session}'
