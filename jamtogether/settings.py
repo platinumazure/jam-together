e989ee978fc0ddf63a4021dd2e2f10d0a3df11f3
Kevin Partington <platinumazure@gmail.com>
2021-07-11 12:10:50 -0500

    Upgrade Django to 3.2.5 and asgiref to 3.4.1

--- jamtogether/settings.py
+++ jamtogether/settings.py
@@ -113,6 +113,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jamtogether.wsgi.application'
 
 
+# Model settings
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
