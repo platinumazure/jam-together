1575e4b7ec469a0a5992643563c9899fa35e7678
Kevin Partington <platinumazure@gmail.com>
2021-09-06 14:43:36 -0500

    Move JamSession models to bottom of file to simplify cross referencing

--- apps/jtapi/models.py
+++ apps/jtapi/models.py
@@ -3,32 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class JamSession(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=2000)
-    conductor = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
-    is_unlisted = models.BooleanField(default=False)
-
-    members = models.ManyToManyField(
-        User,
-        related_name='jam_session_memberships',
-        through='JamSessionMembership'
-    )
-
-    created_by = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class JamSessionMembership(models.Model):
-    jam_session = models.ForeignKey(JamSession, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user} in {self.jam_session}'
-
-
 class SongProvider(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
@@ -107,3 +81,29 @@ class SongPartPage(models.Model):
 
     class Meta:
         order_with_respect_to = 'song_part'
+
+
+class JamSession(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    conductor = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
+    is_unlisted = models.BooleanField(default=False)
+
+    members = models.ManyToManyField(
+        User,
+        related_name='jam_session_memberships',
+        through='JamSessionMembership'
+    )
+
+    created_by = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class JamSessionMembership(models.Model):
+    jam_session = models.ForeignKey(JamSession, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} in {self.jam_session}'
