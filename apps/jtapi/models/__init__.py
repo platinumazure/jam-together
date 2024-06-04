4b709a34847bf295a362f5cf9bd6cd6ec386edeb
platinumazure <platinumazure@gmail.com>
2024-06-04 12:11:33 -0500

    Refactor models (split into song and jam session models)

---
+++ apps/jtapi/models/__init__.py
@@ -0,0 +1,2 @@
+from .song_models import Song, SongProvider, PartDefinition, SongPart, SongPartPage
+from .jam_session_models import JamSession, JamSessionMembership, JamSessionSong
---
+++ apps/jtapi/models/song_models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+class SongProvider(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    site_url = models.URLField()
+
+    # TODO: Add fields for URL patterns for retrieving song info?
+
+    def __str__(self):
+        return f'{self.name} ({self.site_url})'
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100, blank=True)
+    artist = models.CharField(max_length=200, blank=True)
+    composer = models.CharField(max_length=200, blank=True)
+    arranger = models.CharField(max_length=200, blank=True)
+
+    song_provider = models.ForeignKey(
+        SongProvider,
+        on_delete=models.CASCADE,
+        related_name = 'songs',
+    )
+
+    def __str__(self):
+        if self.subtitle:
+            full_title = f'{self.title} ({self.subtitle})'
+        else:
+            full_title = self.title
+
+        return f'{full_title} ({self.song_provider.name})'
+
+
+class PartDefinition(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+
+    class PartTypes(models.TextChoices):
+        INSTRUMENT = 'Instrument'
+        TRANSPOSITION = 'Transposition'
+
+    part_type = models.CharField(max_length=20, choices=PartTypes.choices)
+
+    def __str__(self):
+        return f'{self.name} ({self.part_type})'
+
+
+class SongPart(models.Model):
+    song = models.ForeignKey(
+        Song,
+        on_delete=models.CASCADE,
+        related_name='parts',
+    )
+    definition = models.ForeignKey(
+        PartDefinition,
+        on_delete=models.PROTECT,
+        related_name='+',
+    )
+
+    number = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    reference_url = models.URLField(blank=True)
+    part_file_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.song} ({self.definition.name})'
+
+
+class SongPartPage(models.Model):
+    song_part = models.ForeignKey(
+        SongPart,
+        on_delete=models.CASCADE,
+        related_name='pages',
+    )
+    page_file_url = models.URLField()
+
+    class Meta:
+        order_with_respect_to = 'song_part'
