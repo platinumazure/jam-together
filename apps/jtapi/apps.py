2dfe8e07c6d9680f27bf4131a6f18ef439d72539
platinumazure <platinumazure@gmail.com>
2024-06-10 22:21:07 -0500

    Add label and verbose_name to JTApiConfig
    Also add default_auto_field for safety

--- apps/jtapi/apps.py
+++ apps/jtapi/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class JTApiConfig(AppConfig):
     name = 'apps.jtapi'
+    label = 'jtapi'
+    verbose_name = 'JamTogether API'
+    default_auto_field = 'django.db.models.AutoField'
