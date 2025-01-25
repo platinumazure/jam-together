05c2a97d409beb64d76d5ca136d9c4c20d3194bf
platinumazure <platinumazure@gmail.com>
2025-01-25 11:57:00 -0600

    Use Daphne (ASGI) as development server

--- jamtogether/settings.py
+++ jamtogether/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework_json_api',
     'apps.jtapi.apps.JTApiConfig',
 ]
+
+ASGI_APPLICATION = 'jamtogether.asgi.application'
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
